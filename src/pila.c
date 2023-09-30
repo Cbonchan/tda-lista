6756b2ef55fb7b63f6a0b419b0a853901f17dab9
Cbonchan <basterrasebastian@gmail.com>
2023-09-30 12:49:00 -0300

    Pila O(1) terminada

--- src/pila.c
+++ src/pila.c
@@ -1,35 +1,37 @@
 #include "pila.h"
+#include "lista.h"
 
 pila_t *pila_crear()
 {
-	return NULL;
+	return (pila_t*)lista_crear();
 }
 
 pila_t *pila_apilar(pila_t *pila, void *elemento)
 {
-	return NULL;
+	return (pila_t*)lista_insertar_en_posicion((lista_t*)pila,elemento,0);
 }
 
 void *pila_desapilar(pila_t *pila)
 {
-	return NULL;
+	return lista_quitar_de_posicion((lista_t*)pila,0);
 }
 
 void *pila_tope(pila_t *pila)
 {
-	return NULL;
+	return lista_primero((lista_t*)pila);
 }
 
 size_t pila_tamanio(pila_t *pila)
 {
-	return 0;
+	return lista_tamanio((lista_t*) pila);
 }
 
 bool pila_vacia(pila_t *pila)
 {
-	return false;
+	return lista_vacia((lista_t*)pila);
 }
 
 void pila_destruir(pila_t *pila)
 {
+	lista_destruir((lista_t*)pila);
 }
