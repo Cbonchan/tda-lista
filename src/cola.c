683ee16d4b9ade374ed29d7e52f92135ca2da6ea
Cbonchan <basterrasebastian@gmail.com>
2023-09-30 12:49:30 -0300

    cola terminada

--- src/cola.c
+++ src/cola.c
@@ -1,35 +1,37 @@
 #include "cola.h"
+#include "lista.h"
 
 cola_t *cola_crear()
 {
-	return NULL;
+	return (cola_t*)lista_crear();
 }
 
 cola_t *cola_encolar(cola_t *cola, void *elemento)
 {
-	return NULL;
+	return (cola_t*)lista_insertar((lista_t*)cola,elemento);
 }
 
 void *cola_desencolar(cola_t *cola)
 {
-	return NULL;
+	return lista_quitar_de_posicion((lista_t*)cola, 0);
 }
 
 void *cola_frente(cola_t *cola)
 {
-	return NULL;
+	return lista_primero((lista_t*)cola);
 }
 
 size_t cola_tamanio(cola_t *cola)
 {
-	return 0;
+	return lista_tamanio((lista_t*)cola);
 }
 
 bool cola_vacia(cola_t *cola)
 {
-	return true;
+	return lista_vacia((lista_t*)cola);
 }
 
 void cola_destruir(cola_t *cola)
 {
+	lista_destruir((lista_t*)cola);
 }
